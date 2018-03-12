e016180b151625206192e112bb5ffb7ecbd76385
Randy Reitz <rsreitz@outlook.com>
2018-03-12 12:26:40 -0400

    First pass basic layout.

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    // //  On Click event associated with our choice button function
+    // $("button").on("click", function(event) {
+    //     event.preventDefault();
+    //     doFunction();
+    // });
+
+    // var queryURL = "http://someURL";
+    // $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     })
+    //     .then(function(response) {
+    //         parseResponse(response);
+    //     });
+
+});
+
+function doFunction() {
+
+};
+
+
+function parseResponse(response) {
+
+};
